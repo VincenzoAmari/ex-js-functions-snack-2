cfc41c7f077924e67794c9b033d195347fdff4a9
VincenzoAmari <vincenzo.am99@gmail.com>
2025-04-29 13:44:21 +0200

    snack 2

--- main.js
+++ main.js
@@ -17,3 +17,11 @@ const somma3 = (a, b) => a + b;
 console.log(somma(2, 3));
 console.log(somma2(2, 3));
 console.log(somma3(2, 3));
+
+// Snack 2
+// Crea una arrow function che calcola il quadrato di un numero.
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+const quadrato = (a) => a * a;
+
+console.log(quadrato(6));
