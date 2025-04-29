8cf32e15646be6fe501e546e5778eeb9e6de76e4
VincenzoAmari <vincenzo.am99@gmail.com>
2025-04-29 13:52:47 +0200

    snack 3

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ const somma2 = function (a, b) {
 
 const somma3 = (a, b) => a + b;
 
-console.log(somma(2, 3));
-console.log(somma2(2, 3));
-console.log(somma3(2, 3));
+console.log(somma(2, 3)); //5
+console.log(somma2(2, 3)); //5
+console.log(somma3(2, 3)); //5
 
 // Snack 2
 // Crea una arrow function che calcola il quadrato di un numero.
@@ -24,4 +24,24 @@ console.log(somma3(2, 3));
 
 const quadrato = (a) => a * a;
 
-console.log(quadrato(6));
+console.log(quadrato(6)); //36
+
+// Snack 3
+// Crea una funzione eseguiOperazione
+// Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+const somma4 = (a, b) => a + b;
+const moltipicazione = (a, b) => a * b;
+const sottrazione = (a, b) => a - b;
+const divisione = (a, b) => a / b;
+
+const eseguiOperazione = (a, b, operazione) => operazione(a, b);
+
+console.log(eseguiOperazione(4, 2, somma4)); //6
+console.log(eseguiOperazione(4, 2, moltipicazione)); //8
+console.log(eseguiOperazione(4, 2, sottrazione)); //2
+console.log(eseguiOperazione(8, 2, divisione)); //4
+
+// Snack 4
+// Crea un generatore di funzioni creaTimer
+// Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
